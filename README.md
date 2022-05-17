937748cc3a5676cc2d2f3312a0ed64a6b067cef2
unknown <laziness@protonmail.com>
2022-05-17 18:46:01 -0300

    First commit
    Added first essential assets, all coded by Leanie

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OpenMK
-OpenMK is a development framework for Ikemen GO that brings Mortal Kombat-based functionality to the engine
+OpenMK is a software framework for Ikemen GO engine, based on Mortal Kombat gameplay. This framework intends to offer a set of tools, standards, resources and solutions for making fighting games with Mortal Kombat in mind.
+
+We're following a modular design mindset, where features will have their own place separated from others, will not conflict with each other, and even are usually carried out by different developers. For example, batteplan will exist on the motif-side (coded via LUA), MK-unique lifebar features will be coded in fight.def when possible (and extended via lua when possible, too), and stage fatalities and transitions will belong to each stage and their behavior will be handled via AttachedChars.
+
+# Installing
+To use this framework, make sure you have a clean copy of the latest version available of Ikemen GO. After that, download all the tools you find necessary for your game (or the whole repository to create a full game based on it).
---
+++ data/common.cmd
@@ -0,0 +1,147 @@
+[Command]
+name = "TagShiftBack"
+command = d
+time = 1
+
+[Command]
+name = "TagShiftFwd"
+command = w
+time = 1
+
+[Command]
+name = "x"
+command = x
+time = 1
+
+[Command]
+name = "y"
+command = y
+time = 1
+
+[Command]
+name = "z"
+command = z
+time = 1
+
+[Command]
+name = "a"
+command = a
+time = 1
+
+[Command]
+name = "b"
+command = b
+time = 1
+
+[Command]
+name = "c"
+command = c
+time = 1
+
+[Command]
+name = "start"
+command = s
+time = 1
+
+[Command]
+name = "d"
+command = d
+time = 1
+
+[Command]
+name = "w"
+command = w
+time = 1
+
+[Command]
+name = "m"
+command = m
+time = 1
+
+[Command]
+name = "menu"
+command = m
+time = 1
+
+[Command]
+name = "holdfwd"
+command = /$F
+time = 1
+
+[Command]
+name = "holdback"
+command = /$B
+time = 1
+
+[Command]
+name = "holdup"
+command = /$U
+time = 1
+
+[Command]
+name = "holddown"
+command = /$D
+time = 1
+
+[Command]
+name = "FF"
+command = F,F
+time = 10
+
+[Command]
+name = "BB"
+command = B,B
+time = 10
+
+[Command]
+name = "recovery"
+command = 
+time = 1
+
+; ---------------------------------------------
+; OpenMK-exclusive
+[Command]
+name = "back_z"
+command = /$B,z
+time = 7
+buffer.time = 7
+
+[Command]
+name = "back_z"
+command = B,z
+time = 7
+buffer.time = 7
+
+[Command]
+name = "back_z"
+command = B+z
+time = 7
+buffer.time = 7
+
+[Command]
+name = "back_c"
+command = /$B,c
+time = 7
+buffer.time = 7
+
+[Command]
+name = "back_c"
+command = B,c
+time = 7
+buffer.time = 7
+
+[Command]
+name = "back_c"
+command = B+c
+time = 7
+buffer.time = 7
+
+[Command]
+name = "run"
+command = /y
+time = 1
+
+[Command]
+name = "block"
+command = /b
+time = 1
