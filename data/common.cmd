bdd03a3a43711a0b5aec3e803daf872e2086b652
leanie 3 <83039977+LeanneTM@users.noreply.github.com>
2023-02-26 22:03:56 -0300

    A few more changes
    no voices in common states (you will define them in each char's -3 for better customization), reverted run anim failsafe code, better win pose anim check for when fatalities get started, basic close hits, new constants and other minor changes.

--- data/common.cmd
+++ data/common.cmd
@@ -1,52 +1,4 @@
 ; ---------------------------------------------
-; OpenMK-exclusive
-[Command]
-name = "back_z"
-command = /$B,z
-time = 7
-buffer.time = 7
-
-[Command]
-name = "back_z"
-command = B,z
-time = 7
-buffer.time = 7
-
-[Command]
-name = "back_z"
-command = B+z
-time = 7
-buffer.time = 7
-
-[Command]
-name = "back_c"
-command = /$B,c
-time = 7
-buffer.time = 7
-
-[Command]
-name = "back_c"
-command = B,c
-time = 7
-buffer.time = 7
-
-[Command]
-name = "back_c"
-command = B+c
-time = 7
-buffer.time = 7
-
-[Command]
-name = "run"
-command = /y
-time = 1
-
-[Command]
-name = "block"
-command = /b
-time = 1
-
-; ---------------------------------------------
 ; Common
 [Command]
 name = "recovery"
@@ -147,3 +99,51 @@ time = 10
 name = "BB"
 command = B,B
 time = 10
+
+; ---------------------------------------------
+; OpenMK-exclusive
+[Command]
+name = "back_z"
+command = /$B,z
+time = 7
+buffer.time = 7
+
+[Command]
+name = "back_z"
+command = B,z
+time = 7
+buffer.time = 7
+
+[Command]
+name = "back_z"
+command = B+z
+time = 7
+buffer.time = 7
+
+[Command]
+name = "back_c"
+command = /$B,c
+time = 7
+buffer.time = 7
+
+[Command]
+name = "back_c"
+command = B,c
+time = 7
+buffer.time = 7
+
+[Command]
+name = "back_c"
+command = B+c
+time = 7
+buffer.time = 7
+
+[Command]
+name = "run"
+command = /y
+time = 1
+
+[Command]
+name = "block"
+command = /b
+time = 1
